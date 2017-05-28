954beed897cadbfb57c3ad79c549992c722eb43a
Btanwani77 <Btanwani77@gmail.com>
2017-05-28 21:55:10 +0530

    Greet New user and alert others

--- server/server.js
+++ server/server.js
@@ -13,19 +13,37 @@ io.on('connection',(socket) => {
   console.log('New User Connected');
 
 
-  // socket.emit('newMessage', {
-  //   from:"tanwani@example.com",
-  //   text:"hey whats goin on",
-  //   createdAt:123
-  // });
+  socket.emit('newMessage', {
+    from:"Admin",
+    text:"Welcome To the chat app",
+    createdAt:new Date().getTime()
+
+  });
+  socket.broadcast.emit('newMessage',{
+    from:"Admin",
+    text:"New Used Joined",
+    createdAt:new Date().getTime()
+  });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage',message);
-    io.emit('newMessage',{
+    socket.emit('newMessage',{
       from:message.from,
-      text:message.text,
-      createdAt:new Date().getTime()
-    });
+        text:message.text,
+        createdAt:new Date().getTime()
+    })
+
+    // io.emit('newMessage',{
+    //   from:message.from,
+    //   text:message.text,
+    //   createdAt:new Date().getTime()
+    // });
+// #########################################################
+    // socket.broadcast.emit('newMessage',{
+    //     from:message.from,
+    //     text:message.text,
+    //     createdAt:new Date().getTime()
+    // })
   });
 
   socket.on('disconnect',() => {

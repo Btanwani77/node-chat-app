2fc5a6f7fe09ad80b98d8c984a535ab9eae364a5
Btanwani77 <Btanwani77@gmail.com>
2017-05-29 22:42:23 +0530

    Send Messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -41,11 +41,15 @@ io.on('connection',(socket) => {
   });
 
   socket.on('createMessage', (message,callback) => {
-    console.log('createMessage',message);
-    // socket.emit('newMessage',generateMessage(message.from,message.text));
+    // console.log('createMessage',message);
+    var user = users.getUser(socket.id);
+    if(user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage',generateMessage(user.name,message.text));
 
-    io.emit('newMessage',generateMessage(message.from,message.text));
-    callback('This is from the server');
+    }
+    // socket.emit('newMessage',generateMessage(message.from,message.text));
+    // callback('This is from the server');
+    callback();
     });
 // #########################################################
     // socket.broadcast.emit('newMessage',{
@@ -55,7 +59,11 @@ io.on('connection',(socket) => {
     // })
 
     socket.on('createLocationMessage',(coords) => {
-      io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
+      var user = users.getUser(socket.id);
+      if(user) {
+        io.to(user.room).emit('newLocationMessage',generateLocationMessage(user.name,coords.latitude,coords.longitude));
+
+      }
     });
 
 
